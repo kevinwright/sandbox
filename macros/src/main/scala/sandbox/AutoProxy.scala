a345cb1ab11ff20ed39c16d9393cfee4ecf94a5b
Kevin Wright <kev.lee.wright@gmail.com>
2014-07-01 12:30:21 +0100

    cleaning out the fluff-trap

--- macros/src/main/scala/sandbox/AutoProxy.scala
+++ macros/src/main/scala/sandbox/AutoProxy.scala
@@ -1,9 +1,7 @@
 package sandbox
 
 import scala.language.experimental.macros
-
 import scala.reflect.macros.whitebox.Context
-
 import collection.breakOut
 
 class proxy extends scala.annotation.StaticAnnotation
@@ -28,10 +26,156 @@ object DelegatingMacro {
 
     def showDelegateUsageError(): Unit = log.err("The @delegate annotation can only be used on classes or objects")//params, vals, vars or methods")
 
+    def treeOf[T <: ImplDef](impl: T): T =
+      c.typecheck(impl.duplicate, silent = true, withMacrosDisabled = true).asInstanceOf[T]
+
+    def symOf(impl: ImplDef): TypeSymbol =
+      treeOf(impl).symbol.asType
+
+    def injectMembers(clazz: ClassDef, newMembers: Seq[Tree]): ClassDef = {
+      val ClassDef(mods, name, tparams, Template(parents, self, body)) = clazz
+      ClassDef(mods, name, tparams, Template(parents, self, body ++ newMembers))
+    }
+
+    def hasProxyAnnotation(sym: Symbol): Boolean = sym.annotations.exists(_.toString == classOf[proxy].getName)
+
+    def proxyPivots(sym: TypeSymbol): Seq[Symbol] = {
+      val ctorParams = sym.info.decls collect {
+        case ms: MethodSymbol if ms.name.toString == "<init>" => ms
+      } flatMap(_.paramLists.flatten)
+
+      val candidates = ctorParams ++ sym.info.decls
+
+      candidates.collect{case s: Symbol if hasProxyAnnotation(s) => s}(breakOut)
+    }
+
+    /**
+     * @param origins A mapping from pivot symbols to symbols of provided methods
+     * @return a List of delgating method trees
+     */
+    def mkDelegates(origins: Map[Symbol, Set[MethodSymbol]]): Seq[Tree] = {
+      for {
+        (pivot, methods) <- origins.toSeq
+        method <- methods
+      } yield {
+        val name = method.name.toTermName
+        val paramss = method.paramLists
+        val ret = method.returnType
+
+        val delegateInvocation = {
+          val argss = method.paramLists.map( _.map(param => Ident(param.name)) )
+          q"${pivot.name.toTermName}.${method.name}(...$argss)"
+        }
+
+        val vparamss = method.paramLists.map(_.map {
+          paramSymbol => ValDef(
+            Modifiers(Flag.PARAM, typeNames.EMPTY, List()),
+            paramSymbol.name.toTermName,
+            TypeTree(paramSymbol.typeSignature),
+            EmptyTree)
+        })
+
+        val delegate = q"""def $name(...${vparamss}): $ret = $delegateInvocation"""
+        println("delegate: " + delegate)
+        delegate
+      }
+    }
+
+    def processClass(clazz: ClassDef): ClassDef = {
+      val tcClazz = treeOf(clazz)
+      val sym = tcClazz.symbol.asType
+      val pivots = proxyPivots(sym)
+
+      val existingMethods: Set[MethodSymbol] = sym.info.members.collect{
+        case ms: MethodSymbol => ms
+      }(breakOut)
+
+      val existingInterfaces = sym.info.baseClasses.toSet
+
+      val (existingAbstractMethods, existingConcreteMethods) = existingMethods.partition(_.isAbstract)
+
+      val proxyProvidedMethods: Map[Symbol, Set[MethodSymbol]] = pivots.map{ p =>
+        val methods: Set[MethodSymbol] = p.info.members.collect{
+          case ms: MethodSymbol if !existingConcreteMethods.contains(ms) => ms
+        }(breakOut)
+
+        p -> methods
+      }(breakOut)
+
+      val proxyProvidedInterfaces: Map[Symbol, Set[Symbol]] = pivots.map{ p =>
+        p -> p.info.baseClasses.filter{ !existingInterfaces.contains(_) }.toSet
+      }(breakOut)
+
+      proxyProvidedMethods foreach { case (pivot,methods) =>
+        println(s"PPM for ${pivot.name} = ${methods.mkString}")
+      }
+
+      val delegates: Seq[Tree] = mkDelegates(proxyProvidedMethods)
+      injectMembers(clazz, delegates)
+    }
+
+    def processModule(mod: ModuleDef): ModuleDef = {
+      val sym = symOf(mod)
+      mod
+    }
+
+    val inputs = annottees.map(_.tree).toList
+    
+    val expandees = inputs match {
+      case (clazz: ClassDef) :: rest      => processClass(clazz) :: rest
+      case (singleton: ModuleDef) :: rest => processModule(singleton) :: rest
+      case _                              => showDelegateUsageError(); inputs
+    }
+    val outputs = expandees
+    c.Expr[Any](Block(outputs, Literal(Constant(()))))
+  }
+}
+
+
+/*
+
+OLD EXPERIMENTS:
+
+This one may be needed if Scopes.unlink NPE can only be fixed by not typechecking the class
+
+    def proxyPivots(impl: ImplDef): List[Tree] = {
+
+      val ProxyAnnotationRegex="""new .*proxy\(\)""".r
+
+      def hasProxyAnnotation(m: MemberDef): Boolean = m.mods.annotations.exists{
+        case x if ProxyAnnotationRegex.findFirstIn(x.toString).isDefined => true
+        case x =>
+          log.info(s"bad annotation: [${x.toString}]")
+          false
+      }
+
+      impl.impl.body collect {
+//        case m: MemberDef if hasProxyAnnotation(m) => m
+        case m: MemberDef =>
+          log.info("mod annotations on [" + m.toString + "] = " + m.mods.annotations.mkString)
+          log.info("sym annotations on [" + m.toString + "] = " + m.symbol.annotations.mkString)
+          m
+
+        case m =>
+          log.info("sym annotations on [" + m.toString + "] = " + m.symbol.annotations.mkString)
+          m
+      }
+    }
+
+
+
+-----------------------------------------------------
 
     val typeDiscovery = new { val ctx: c.type = c } with TypeDiscovery
     import typeDiscovery._
-    
+
+-----------------------------------------------------
+
+    def methodsIn(body: List[Tree]): Set[DefDef] =
+      body.flatMap{ case dd : DefDef => List(dd); case _ => Nil }(collection.breakOut)
+
+-----------------------------------------------------
+
     /**
      * @param methodSym: Symbol of the method (or accessor) being delegated
      * @param proxy: The member of the delegating class that holds the object instance being delegated to
@@ -48,12 +192,9 @@ object DelegatingMacro {
           EmptyTree)
       })
 
-
-
 //      log.rawInfo("methodSym.typeParams", methodSym.typeParams)
 //      log.rawInfo("methodSym.paramss", methodSym.paramss)
 //      log.rawInfo("vparamss", vparamss)
-
 
       // TODO: multi params list
       val delegateInvocation = {
@@ -77,22 +218,7 @@ object DelegatingMacro {
       synDef
     }
 
-    def methodsIn(body: List[Tree]): Set[DefDef] =
-      body.flatMap{ case dd : DefDef => List(dd); case _ => Nil }(collection.breakOut)
-
-    def treeOf[T <: ImplDef](impl: T): T = {
-      val out = ctx.typecheck(impl.duplicate, silent = true, withMacrosDisabled = true)
-      out.asInstanceOf[T]
-    }
-
-    def symOf(impl: ImplDef): TypeSymbol =
-      treeOf(impl).symbol.asType
-
-//    def classSymOf(clazz: ClassDef): ClassSymbol =
-//      treeOf(clazz).symbol.asClass
-//
-//    def moduleSymOf(mod: ModuleDef): ModuleSymbol =
-//      treeOf(mod).symbol.asModule
+-----------------------------------------------------
 
     //TODO: Isolate logic that injects into a ClassDef
     //  If delegating via a non-param member then we won't have this surrounding
@@ -176,141 +302,21 @@ object DelegatingMacro {
       ClassDef(mods, name, tparams, Template(parents, self, body ++ newMethods))
     }
 
+-----------------------------------------------------
 
-//    def proxyPivots(impl: ImplDef): List[Tree] = {
-//
-//      val ProxyAnnotationRegex="""new .*proxy\(\)""".r
-//
-//      def hasProxyAnnotation(m: MemberDef): Boolean = m.mods.annotations.exists{
-//        case x if ProxyAnnotationRegex.findFirstIn(x.toString).isDefined => true
-//        case x =>
-//          log.info(s"bad annotation: [${x.toString}]")
-//          false
-//      }
-//
-//      impl.impl.body collect {
-////        case m: MemberDef if hasProxyAnnotation(m) => m
-//        case m: MemberDef =>
-//          log.info("mod annotations on [" + m.toString + "] = " + m.mods.annotations.mkString)
-//          log.info("sym annotations on [" + m.toString + "] = " + m.symbol.annotations.mkString)
-//          m
-//
-//        case m =>
-//          log.info("sym annotations on [" + m.toString + "] = " + m.symbol.annotations.mkString)
-//          m
-//      }
-//    }
+Useful for input matching on a naked @proxy:
 
-    def injectMembers(clazz: ClassDef, newMembers: Seq[Tree]): ClassDef = {
-      val ClassDef(mods, name, tparams, Template(parents, self, body)) = clazz
-      ClassDef(mods, name, tparams, Template(parents, self, body ++ newMembers))
-    }
-
-    def proxyPivots(sym: TypeSymbol): Seq[Symbol] = {
-
-      def hasProxyAnnotation(sym: Symbol): Boolean =
-        sym.annotations.exists(_.toString == classOf[proxy].getName)
-
-      val ctorParams = sym.info.decls collect {
-        case ms: MethodSymbol if ms.name.toString == "<init>" => ms
-      } flatMap(_.paramLists.flatten)
-
-      val candidates = ctorParams ++ sym.info.decls
-
-      candidates.collect{
-        case s: Symbol if hasProxyAnnotation(s) => s
-      }.toSeq
-    }
-
-    def processClass(clazz: ClassDef): ClassDef = {
-      val tcClazz = treeOf(clazz)
-      val sym = tcClazz.symbol.asType
-      val pivots = proxyPivots(sym)
-
-      val existingMethods = sym.info.members.collect{
-        case ms: MethodSymbol => ms
-      }.toSet
-
-      val existingInterfaces = sym.info.baseClasses.toSet
-
-      val (existingAbstractMethods, existingConcreteMethods) = existingMethods.partition(_.isAbstract)
-
-      val proxyProvidedMethods: Map[Symbol, Set[MethodSymbol]] = pivots.map{ p =>
-        p -> p.info.members.collect{
-          case ms: MethodSymbol if !existingConcreteMethods.contains(ms) => ms
-        }.toSet
-      }(breakOut)
-
-      val proxyProvidedInterfaces: Map[Symbol, Set[Symbol]] = pivots.map{ p =>
-        p -> p.info.baseClasses.filter{ !existingInterfaces.contains(_) }.toSet
-      }(breakOut)
-
-      proxyProvidedMethods foreach { case (pivot,methods) =>
-        println(s"PPM for ${pivot.name} = ${methods.mkString}")
-      }
-
-      val delegates: Seq[Tree] = {
-        for {
-          (pivot, methods) <- proxyProvidedMethods.toSeq
-          method <- methods
-        } yield {
-          val name = method.name.toTermName
-          val paramss = method.paramLists
-          val ret = method.returnType
-
-          val delegateInvocation = {
-            val argss = method.paramLists.map( _.map(param => Ident(param.name)) )
-            q"${pivot.name.toTermName}.${method.name}(...$argss)"
-          }
-
-          val vparamss = method.paramLists.map(_.map {
-            paramSymbol => ValDef(
-              Modifiers(Flag.PARAM, typeNames.EMPTY, List()),
-              paramSymbol.name.toTermName,
-              TypeTree(paramSymbol.typeSignature),
-              EmptyTree)
-          })
-
-          val delegate = q"""def $name(...${vparamss}): $ret = $delegateInvocation"""
-          println("delegate: " + delegate)
-          delegate
-        }
-      }
-
-      injectMembers(clazz, delegates)
-    }
-
-    def processModule(mod: ModuleDef): ModuleDef = {
-      val sym = symOf(mod)
-      mod
-    }
-
-    val inputs = annottees.map(_.tree).toList
-    
-    val (annottee, expandees) = inputs match {
-      case (clazz: ClassDef) :: rest =>
-        (EmptyTree, processClass(clazz) :: rest)
-
-      case (singleton: ModuleDef) :: rest =>
-        (EmptyTree, processModule(singleton) :: rest)
-
-//      case (param: ValOrDefDef) :: (enclosing: ClassDef) :: rest =>
-//        //We're a param, and the ClassDef is supplied
-////        val newEnclosing = addDelegateMethods(param, enclosing)
-////        (param, newEnclosing :: rest)
-//        showDelegateUsageError()
-//        (EmptyTree, inputs)
-//
-//      case (param: ValOrDefDef) :: Nil =>
-//        //We're a member, and must work out who owns us. Have to warn of reduced functionality
-//        showDelegateUsageError()
-//        (EmptyTree, inputs)
-        
-      case _ =>
+      case (param: ValOrDefDef) :: (enclosing: ClassDef) :: rest =>
+        //We're a param, and the ClassDef is supplied
+//        val newEnclosing = addDelegateMethods(param, enclosing)
+//        (param, newEnclosing :: rest)
         showDelegateUsageError()
         (EmptyTree, inputs)
-    }
-    val outputs = expandees
-    c.Expr[Any](Block(outputs, Literal(Constant(()))))
-  }
-}
+
+      case (param: ValOrDefDef) :: Nil =>
+        //We're a member, and must work out who owns us. Have to warn of reduced functionality
+        showDelegateUsageError()
+        (EmptyTree, inputs)
+
+
+ */
